fd05ce76907f62efa0ee3e745c43c6c7c5222a54
lugo <lugo@yandex-team.ru>
2019-01-20 07:07:37 +0300

    mysql lib

---
+++ src/protocol.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Yuriy Baranov on 2019-01-20.
+//
+
+#include "protocol.h"
---
+++ src/protocol.h
@@ -0,0 +1,8 @@
+//
+// Created by Yuriy Baranov on 2019-01-20.
+//
+
+#ifndef PROJECT_PROTOCOL_H
+#define PROJECT_PROTOCOL_H
+
+#endif //PROJECT_PROTOCOL_H
